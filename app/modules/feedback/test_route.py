1168b1f2adecd77f1d0d41f3a28bca91073a7598
Michael Erdenberger <merdenbe@nd.edu>
2019-07-19 11:50:30 -0700

    testing complete on the secure sections of the api

--- app/modules/feedback/test_route.py
+++ app/modules/feedback/test_route.py
@@ -1,6 +1,7 @@
 import requests
 import ujson
 import unittest
+import os
 
 
 STAGING_URL = 'https://aris-backend-staging.herokuapp.com/feedback'
@@ -9,22 +10,34 @@ STAGING_URL = 'https://aris-backend-staging.herokuapp.com/feedback'
 class TestFeedback(unittest.TestCase):
 
     def test_no_body(self):
-        r = requests.post(url=STAGING_URL)
+        headers = {'Authorization': 'Bearer ' + os.environ["TEST_TOKEN"]}
+        r = requests.post(url=STAGING_URL, headers=headers)
 
         self.assertTrue(r.status_code, 404)
 
     def test_incorrect_params(self):
+        headers = {'Authorization': 'Bearer ' + os.environ["TEST_TOKEN"]}
         body = {
             "message": "This is a test of the route."
         }
-        r = requests.post(url=STAGING_URL, data=ujson.dumps(body))
+        r = requests.post(url=STAGING_URL, data=ujson.dumps(body), headers=headers)
 
         self.assertTrue(r.status_code, 404)
 
-    def test_valid_post(self):
+    def test_unathorized_post(self):
+        headers = {'Authorization': 'Bearer ' + '1235467800'}
         body = {
             "msg": "This is a test of the route."
         }
-        r = requests.post(url=STAGING_URL, data=ujson.dumps(body))
+        r = requests.post(url=STAGING_URL, data=ujson.dumps(body), headers=headers)
+
+        self.assertTrue(r.status_code, 200)
+
+    def test_valid_post(self):
+        headers = {'Authorization': 'Bearer ' + os.environ["TEST_TOKEN"]}
+        body = {
+            "msg": "This is a test of the route."
+        }
+        r = requests.post(url=STAGING_URL, data=ujson.dumps(body), headers=headers)
 
         self.assertTrue(r.status_code, 200)
--- app/modules/majors/resources.py
+++ app/modules/majors/resources.py
@@ -1,13 +1,10 @@
 from .db import db
-from ..reauthenticate.authenticate import authenticate_request
 
 
 class MajorResource:
 
     # Return a list of majors for a given university
     def on_get(self, req, resp):
-        authenticate_request(req, self.Api_Session)
-
         majors = self.db.listMajors()
 
         resp.media = {'majors': majors}
